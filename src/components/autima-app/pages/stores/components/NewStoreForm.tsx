f8c437774d376c7185bbba1bfb91729f69e48c8d
mtabech21 <mtabech21@gmail.com>
2023-02-07 00:04:05 -0500

    alpha 0.0.0.1

--- src/components/autima-app/pages/stores/components/NewStoreForm.tsx
+++ src/components/autima-app/pages/stores/components/NewStoreForm.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { IoAddOutline, IoClose, IoExit } from 'react-icons/io5'
 import styles from "../stores.module.scss"
 
-function NewStoreForm({closeButtonDo}) {
+interface NewStoreFormProps {
+  closeButtonDo: ()=>void
+}
+
+function NewStoreForm(props: NewStoreFormProps) {
   return (
     <div className={ styles.storeForm}>
       <div className={styles.topForm}>
-        <IoClose onClick={closeButtonDo} className={styles.closeBtn}/>
+        <IoClose onClick={props.closeButtonDo} className={styles.closeBtn}/>
         <div className={styles.formTitle}>Add New Branch</div>
         <IoClose className={styles.closeBtn} style={{opacity: "0"}}/>
       </div>
